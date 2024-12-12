0444faf9795ccbc63ed602445b963e0af205dae2
ABCSDFWLO <76253761+ABCSDFWLO@users.noreply.github.com>
2024-12-12 14:00:26 +0900

    🗃️add index constraint to User name & email field
    and rename User password to password_hash

--- server/main.py
+++ server/main.py
@@ -28,9 +28,9 @@ class ActionType(Enum):
 
 class User(SQLModel, table=True):
     id: int = Field(primary_key=True)
-    name: str = Field(unique=True)
-    email: str = Field(unique=True)
-    password: str
+    name: str = Field(index=True,unique=True)
+    email: EmailStr = Field(index=True,unique=True)
+    password_hash: str
     registered_at: datetime
 class Game(SQLModel, table=True):
     id: int = Field(primary_key=True)
