6531a293531ed55a082baf722496fe8e0bb96e91
ABCSDFWLO <76253761+ABCSDFWLO@users.noreply.github.com>
2024-12-14 02:27:07 +0900

    ✨add GET : verify

--- server/main.py
+++ server/main.py
@@ -1,10 +1,13 @@
 from typing import Annotated, List, Union
-from fastapi import Depends, FastAPI, HTTPException, Header
+from uuid import uuid4, UUID
+from fastapi import Body, Depends, FastAPI, HTTPException, Header, Path
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from sqlmodel import Session, select
 from sqlalchemy.exc import IntegrityError
 from passlib.hash import bcrypt
+import random
+import string
 
 from db import engine, create_db_and_tables, User, Game, Action, State, ActionType
 from token_manager import get_token_manager, user_id_from_token
@@ -29,6 +32,11 @@ class UserCreate(BaseModel):
 class UserLogin(BaseModel):
     email : EmailStr
     password : str
+
+unverified_users = { UUID:(User,str) }
+def generate_verification_code(length:int)->str:
+    charset = string.ascii_lowercase + string.digits
+    return ''.join(random.choices(charset, k=length))
 
 @app.get("/users", response_model=List[UserOut])
 async def get_users():
@@ -56,23 +64,46 @@ async def get_user(user_name: str):
             registered_at=result.registered_at
             )
 
-@app.post("/register", status_code=201, response_model=int)
-async def create_user(user_create: UserCreate):
+@app.post("/register", status_code=200, response_model=UUID)
+async def create_user(user_create: UserCreate, code:str=Depends(generate_verification_code)):
     with Session(engine) as session:
+        email=session.get(User, user_create.email)
+        if email is not None:
+            raise HTTPException(status_code=400, detail="Email already exists")
+        name=session.get(User, user_create.name)
+        if name is not None:
+            raise HTTPException(status_code=400, detail="Name already exists")
         user = User(
             name=user_create.name,
             email=user_create.email,
             password_hash=bcrypt.hash(user_create.password),
             registered_at=datetime.now()
         )
+        uuid = uuid4()
+        unverified_users[uuid] = (user, generate_verification_code(6)) # TODO : seperate Constants
+        return uuid
+        
+@app.post("/verify/{uuid}", status_code=201, response_model=int)
+async def verify_user(uuid: UUID=Path(...), verify_code: str = Body(len=6)):
+    with Session(engine) as session:
+        unverified_user_record = unverified_users.get(uuid) # Tuple, [0]: User, [1]: Verify Code
+        if unverified_user_record is None:
+            raise HTTPException(status_code=404, detail="Invalid UUID")
+        user = unverified_user_record[0]
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        if unverified_user_record[1] != verify_code:
+            raise HTTPException(status_code=400, detail="Invalid verification code")
+        user.registered_at = datetime.now()
         session.add(user)
         try:
             session.commit()
             session.refresh(user)
         except IntegrityError:
             session.rollback()
-            raise HTTPException(status_code=400, detail="Email or name already exists")
+            raise HTTPException(status_code=500, detail="Database error")
         else:
+            del unverified_users[uuid]
             return user.id
         
 @app.post("/login", status_code=200, response_model=List[str])
