3b1ee1c5a8a8a1f8749800dc459b8e8c4b978b3e
ABCSDFWLO <76253761+ABCSDFWLO@users.noreply.github.com>
2024-12-19 06:05:28 +0900

    ✨add scene : main

--- client/vyex/scenes/title_manager.gd
+++ client/vyex/scenes/title_manager.gd
@@ -1,3 +1,12 @@
 extends Control
 
 @onready var logo_stroke := $LogoStroke
+@onready var main_enter_button := $LogoStroke/MainEnterButton
+@onready var login_form := $LoginForm
+
+func _on_main_enter_button_pressed() -> void:
+	main_enter_button.visible=false
+	var tween = get_tree().create_tween()
+	tween.tween_property(logo_stroke,"position",logo_stroke.position+Vector2.LEFT*250,0.5).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(login_form,"modulate",Color(1,1,1,1),0.5).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	
