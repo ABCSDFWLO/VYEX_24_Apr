d3784eb68556b7cde3056e4b6556b61b6567069e
ABCSDFWLO <76253761+ABCSDFWLO@users.noreply.github.com>
2024-12-29 02:31:15 +0900

    ✨add lobby scene

--- client/vyex/scripts/scene_manager.gd
+++ client/vyex/scripts/scene_manager.gd
@@ -1,1 +1,11 @@
 extends Node
+
+@onready var title : Control = $Title
+
+@onready var lobby_resource = preload("res://scenes/lobby.tscn")
+
+
+
+func _on_control_login(tokens: Array) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(title,"modulate",Color(1,1,1,1),0.5).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
