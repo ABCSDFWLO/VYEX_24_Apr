bf4dd9f09cd7ecc26c6cf70dfcb6131a7b43a8aa
ABCSDFWLO <76253761+ABCSDFWLO@users.noreply.github.com>
2024-12-29 15:24:39 +0900

    ✨add lobby manager

---
+++ client/vyex/scripts/lobby_manager.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var rooms : Array[Dictionary]
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func render()->void:
+	pass
