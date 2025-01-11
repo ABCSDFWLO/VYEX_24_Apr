4748720298fd92fa66974601a60275798f2e8185
ABCSDFWLO <76253761+ABCSDFWLO@users.noreply.github.com>
2025-01-12 02:41:08 +0900

    ✨add map_shape row/col adjust feature

--- client/vyex/scripts/map_shape_container.gd
+++ client/vyex/scripts/map_shape_container.gd
@@ -14,30 +14,25 @@ extends Container
 @onready var icon_yzav_red := preload("res://assets/sprites/icon_yzav_red.png")
 @onready var icon_none := preload("res://assets/sprites/icon_none.png")
 
-# Called when the node enters the scene tree for the first time.
+var row := 7
+var col := 7
+
 func _ready() -> void:
 	_reset()
 
 func _reset() -> void:
+	row=7
+	col=7
 	map_width_spinbox.value=7
 	map_height_spinbox.value=7
+	map_shape_grid_container.columns=7
 	var children = map_shape_grid_container.get_children()
 	for child in children:
 		map_shape_grid_container.remove_child(child)
 		child.queue_free()
 	for i in 7:
 		for j in 7:
-			var option_button = OptionButton.new()
-			option_button.add_icon_item(icon_floor,"")
-			option_button.add_icon_item(icon_xaht_blue,"")
-			option_button.add_icon_item(icon_vusu_blue,"")
-			option_button.add_icon_item(icon_ewng_blue,"")
-			option_button.add_icon_item(icon_yzav_blue,"")
-			option_button.add_icon_item(icon_xaht_red,"")
-			option_button.add_icon_item(icon_vusu_red,"")
-			option_button.add_icon_item(icon_ewng_red,"")
-			option_button.add_icon_item(icon_yzav_red,"")
-			option_button.add_icon_item(icon_none,"")
+			var option_button : OptionButton = _new_option_button()
 			if i==0 and j==0:
 				option_button.select(4)
 			elif i==0 and j==2 or i==2 and j==0:
@@ -58,13 +53,55 @@ func _reset() -> void:
 				option_button.select(9)
 			map_shape_grid_container.add_child(option_button)
 
+func _new_option_button() -> OptionButton:
+	var option_button = OptionButton.new()
+	option_button.add_icon_item(icon_floor,"")
+	option_button.add_icon_item(icon_xaht_blue,"")
+	option_button.add_icon_item(icon_vusu_blue,"")
+	option_button.add_icon_item(icon_ewng_blue,"")
+	option_button.add_icon_item(icon_yzav_blue,"")
+	option_button.add_icon_item(icon_xaht_red,"")
+	option_button.add_icon_item(icon_vusu_red,"")
+	option_button.add_icon_item(icon_ewng_red,"")
+	option_button.add_icon_item(icon_yzav_red,"")
+	option_button.add_icon_item(icon_none,"")
+	return option_button
 
 func _on_map_size_reset_button_pressed() -> void:
 	_reset()
 
 func _on_map_width_spin_box_value_changed(value: float) -> void:
-	pass # Replace with function body.
+	if col>value:
+		for c in col-value:
+			for r in row:
+				var i := col*r+col-r-1
+				var child : OptionButton = map_shape_grid_container.get_child(i)
+				map_shape_grid_container.remove_child(child)
+				child.queue_free()
+			col -= 1
+			map_shape_grid_container.columns=col
+	elif col<value:
+		for c in value-col:
+			for r in row:
+				var i := (col+1)*(r+1)-1
+				var new_child : OptionButton = _new_option_button()
+				map_shape_grid_container.add_child(new_child)
+				map_shape_grid_container.move_child(new_child,i)
+			col += 1
+			map_shape_grid_container.columns=col
 
 
 func _on_map_height_spin_box_value_changed(value: float) -> void:
-	pass # Replace with function body.
+	if row>value:
+		for r in row-value:
+			for c in col:
+				var child : OptionButton = map_shape_grid_container.get_child(-1)
+				map_shape_grid_container.remove_child(child)
+				child.queue_free()
+			row -= 1
+	elif row<value:
+		for r in value-row:
+			for c in col:
+				var new_child : OptionButton = _new_option_button()
+				map_shape_grid_container.add_child(new_child)
+			row += 1
