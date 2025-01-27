41f1e5b41422975b3b08baa4f365e074f307ae69
ABCSDFWLO <76253761+ABCSDFWLO@users.noreply.github.com>
2025-01-28 00:40:46 +0900

    🗃️add Initial_game_setting

--- server/db.py
+++ server/db.py
@@ -5,6 +5,7 @@ from pydantic import EmailStr
 from datetime import datetime
 from enum import Enum
 
+default_pann_code = b'\x41\x01\x21\x01\x01\x00\x00\x01\x31\x01\x01\x01\x01\x00\x21\x01\x11\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x51\x01\x61\x00\x01\x01\x01\x01\x71\x01\x00\x00\x01\x01\x61\x01\x81'
 
 class GameState(Enum):
     Running = "Running"
@@ -23,7 +24,6 @@ class ActionType(Enum):
     Vusu = "Vusu"
     Win_Yzav = "Win_Yzav"
     Lose_Makk = "Lose_Makk"
-    Undo = "Undo"
     Chuwm_Xaht = "Chuwm_Xaht"
 
 class User(SQLModel, table=True):
@@ -36,16 +36,37 @@ class User(SQLModel, table=True):
     games_as_player1: list["Game"] = Relationship(back_populates="player1", sa_relationship=relationship("Game", foreign_keys="[Game.player1_id]"))
     games_as_player2: list["Game"] = Relationship(back_populates="player2", sa_relationship=relationship("Game", foreign_keys="[Game.player2_id]"))
     actions: list["Action"] = Relationship(back_populates="player")
+class InitialGameSetting(SQLModel, table=True):
+    __tablename__ = "initial_game_setting"
+
+    id: int = Field(primary_key=True)
+    pann_size_x: int = Field(default=7, ge=2, le=64)
+    pann_size_y: int = Field(default=7, ge=2, le=64)
+    pann_code: bytes = Field(default=default_pann_code)
+    """
+    byteStream of the pann.
+    Each byte represents a cell in the pann.
+    LSB[0~3] : wall (0~15 heights)
+    LSB[4~7] : maal (0b0000 : No Maal, 0b0001 : XAHT_BLUE, 0b0010 : VUSU_BLUE, 0b0011 : EWNG_BLUE, 0b0100 : YZAV_BLUE, 0b0101 : XAHT_RED, 0b0110 : VUSU_RED, 0b0111 : EWNG_RED, 0b1000 : YZAV_RED)
+    ex) 0b00000000 : empty cell, 0b01010011 : XAHT_RED on the 3-height wall
+    """
+    action_point_max: int = Field(default=3, ge=1, le=32)
+    action_point_advantage_for_p1: int = Field(default=0, ge=-32, le=32)
+    action_point_advantage_for_p2: int = Field(default=1, ge=-32, le=32)
+    
+    games: list["Game"] = Relationship(back_populates="initial_setting")
 class Game(SQLModel, table=True):
     id: int = Field(primary_key=True)
     state: GameState
     name: str
     started_at: datetime
     ended_at: datetime | None
+    initial_setting_id: int = Field(foreign_key="initial_game_setting.id")
     player1_id: int | None = Field(foreign_key="user.id")
     player2_id: int | None = Field(foreign_key="user.id")
     password_hash: str | None
 
+    initial_setting: InitialGameSetting = Relationship(back_populates="games")
     player1: User = Relationship(back_populates="games_as_player1", sa_relationship=relationship("User", foreign_keys="[Game.player1_id]"))
     player2: User | None = Relationship(back_populates="games_as_player2", sa_relationship=relationship("User", foreign_keys="[Game.player2_id]"))
     actions: list["Action"] = Relationship(back_populates="game")
